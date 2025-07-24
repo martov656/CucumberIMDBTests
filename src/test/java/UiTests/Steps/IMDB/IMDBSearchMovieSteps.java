45d08755a375a15d0410d0fd6df86826c0fbbfa5
martov656 <martintovarek2@gmail.com>
2025-07-24 20:58:34 +0200

    imdb

---
+++ src/test/java/UiTests/Steps/IMDB/IMDBSearchMovieSteps.java
@@ -0,0 +1,93 @@
+package UiTests.Steps.IMDB;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+
+public class IMDBSearchMovieSteps {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public void acceptCookiesIfVisible() {
+        try {
+            WebDriverWait shortWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            WebElement acceptCookies = shortWait.until(ExpectedConditions.elementToBeClickable(
+                    By.xpath("//button[contains(text(),'Accept') or contains(text(),'Souhlasím') or contains(text(),'Rozumím')]")
+            ));
+            acceptCookies.click();
+            System.out.println("Cookies byly akceptovány.");
+        } catch (TimeoutException e) {
+            System.out.println("Cookies banner se nezobrazil nebo už byl potvrzen.");
+        }
+    }
+
+    @Given("user opens movie database home page")
+    public void openMovieDatabaseHomepage() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://www.imdb.com/");
+
+        acceptCookiesIfVisible();
+    }
+
+    @When("user is searching for the following movies:")
+    public void userSearchesForMovies(DataTable dataTable) {
+        List<String> movies = dataTable.asList();
+
+        for (String movie : movies) {
+            System.out.println("Searching for: " + movie);
+
+            // Vyhledání pole a zadání jména
+            WebElement searchBox = wait.until(ExpectedConditions.elementToBeClickable(By.name("q")));
+            searchBox.clear();
+            searchBox.sendKeys(movie);
+            searchBox.sendKeys(Keys.ENTER);
+
+            // Počkej na výsledek a klikni na něj
+            WebElement result = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                    By.xpath("//a[contains(., '" + movie + "')]")
+            ));
+            Assertions.assertTrue(result.isDisplayed(), "Result not found for: " + movie);
+            result.click();
+
+            // Ověř, že stránka obsahuje jméno
+            wait.until(ExpectedConditions.titleContains(movie));
+            Assertions.assertTrue(driver.getPageSource().contains(movie), "Name not found on page: " + movie);
+
+            // Po kliknutí na herce můžeme zavolat pomocné metody (nebo přidat další kroky v Gherkinu)
+            clickTrailerLink();
+
+            // Po skončení se vrať zpět na homepage pro další jméno
+            driver.get("https://www.imdb.com/");
+        }
+    }
+
+
+    public void clickTrailerLink() {
+        WebElement trailerLink = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.cssSelector("a.ipc-lockup-overlay[aria-label^='Watch']")
+        ));
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", trailerLink);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", trailerLink);
+    }
+
+    @Then("imdb browser is closed")
+    public void closeImdbBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
--- src/test/resources/Features/IMDB/IMDBSearch.feature
+++ src/test/resources/Features/IMDB/IMDBSearch.feature
@@ -19,3 +19,51 @@ Feature: IMDb celebrity search with multiple names
 
     Then the browser is closed
 
+  @multiple2
+  Scenario: User searches for multiple movies on IMDb
+    Given user opens movie database home page
+    When user is searching for the following movies:
+      | Dr. No             |
+      | Gravitace          |
+      | Match Point        |
+      | Šílený Max 2: Bojovník silnic|
+      | Sedm               |
+      |Expendables: Postradatelní 2|
+      |Vetřelec 3|
+      |Letiště|
+      |Airport 1975|
+      |Osvícení|
+      |Rocky|
+
+    Then imdb browser is closed
+
+  @multiple3
+  Scenario: User searches for multiple Reese Witherspoon movies on IMDb
+    Given user opens movie database home page
+    When user is searching for the following movies:
+      | Pravá blondýnka    |
+      |Poznáš, až to přijde?|
+      |Voda pro slony|
+      |The Morning Show|
+      |Tohle je válka!|
+      |Který je ten pravý?|
+      |Sedmilhářky|
+      |Jarmark marnosti|
+      |Sedmilhářky|
+
+    Then imdb browser is closed
+
+  @multiple4
+  Scenario: User searches for multiple Reese movies on IMDb
+    Given user opens movie database home page
+    When user is searching for the following movies:
+      |Poznáš, až to přijde?|
+      |Voda pro slony|
+      |The Morning Show|
+      |Tohle je válka!|
+      |Který je ten pravý?|
+      |Sedmilhářky|
+      |Jarmark marnosti|
+      |Sedmilhářky|
+
+    Then imdb browser is closed
